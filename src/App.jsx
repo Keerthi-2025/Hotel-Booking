5ae3975c260880bb4beeac49af59b498f81a6dad
Keerthi <rhkeerthi3@gmail.com>
2025-08-22 13:28:21 +0530

    added pages for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import {BrowserRouter, Route, Routes,} from "react-router-dom";
 
 import "./index.css";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 export default function () {
   return (
-  <h1 className="text-red-600">
-  App
-</h1>
+  
+  <BrowserRouter>
+  <Routes>
+    <Route path ="about" element = {<About/>} />
+    <Route path = "/" element = {<Home/>}/>
+    <Route path ="profile" element = {<Profile/>} />
+    <Route path = "sign-in" element = {<SignIn/>}/>
+    <Route path ="signup" element = {<SignUp/>} />
+   
+
+
+  </Routes>
+  
+  
+  </BrowserRouter>
 
   )
 }

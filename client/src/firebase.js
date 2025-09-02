d25436328f278f606ad3d76b5e295e35391b80cd
Keerthi <rhkeerthi3@gmail.com>
2025-09-02 15:48:51 +0530

    complete project

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqgRmZWAXXSejKF54ZDaAtYteT8GKEaYs",
+  authDomain: "hotel-booking-884d1.firebaseapp.com",
+  projectId: "hotel-booking-884d1",
+  storageBucket: "hotel-booking-884d1.firebasestorage.app",
+  messagingSenderId: "178799629711",
+  appId: "1:178799629711:web:cd3b7634a83df9d72127fa"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,17 @@
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Heritage Haven</h1>
+      <p className='mb-4 text-slate-700'>
+        Heritage Haven is a premier real estate firm specializing in exclusive properties and refined living experiences. We cater to discerning clients who value sophistication, discretion, and exceptional service in every transaction.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our mission is to redefine the standard of real estate by blending market expertise with a bespoke, client-focused approach. From luxury residences to high-value investments, we provide curated opportunities that align with your vision and lifestyle.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        With an elite team of professionals and unparalleled knowledge of the market, Oii Estate is committed to delivering excellence at every step. For us, real estate is not just about property — it’s about creating lasting value, prestige, and opportunity.
+      </p>
+    </div>
+  )
+}
